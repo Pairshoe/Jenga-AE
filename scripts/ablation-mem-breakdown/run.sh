d40682fac4c03aa8713b1b714f8059156261a61c
ssttaarr123 <1458727082@qq.com>
2025-05-10 14:46:18 +0800

    small improve

--- scripts/ablation-mem-breakdown/run.sh
+++ scripts/ablation-mem-breakdown/run.sh
@@ -7,6 +7,7 @@ bash scripts/ablation-mem-breakdown/llama-jenga.sh llama2 12288
 bash scripts/ablation-mem-breakdown/llama-jenga.sh llama2 14336
 bash scripts/ablation-mem-breakdown/llama-jenga.sh llama2 16384
 
+bash scripts/ablation-mem-breakdown/predictor.sh
 
 mkdir -p output_figures/ablations/memory-breakdown
 python src/experiment/ablation/memory-breakdown/plot.py
--- src/experiment/ablation/memory-breakdown/test_predictor_mem.py
+++ src/experiment/ablation/memory-breakdown/test_predictor_mem.py
@@ -9,28 +9,17 @@ if __name__ == "__main__":
     parser.add_argument("--predictor_path", type=str, default="checkpoints/predictor")
     args = parser.parse_args()
     
-    pruned_cfg = torch.load(os.path.join(args.predictor_path, "pruned_config.pth"))
-    layers_cfg = pruned_cfg["layers"]
-    
-    predictors = []
-    total_params = 0
 
-    for layer in range(len(layers_cfg)):
-        layer_cfg = layers_cfg[layer]
-        predictor = PrunableAttnPredictorInfer(dim=128,
-                                               hidden_dim=512,
-                                               q1_outdim=layer_cfg["q1_outdim"],
-                                               q2_outdim=layer_cfg["q2_outdim"],
-                                               k1_outdim=layer_cfg["k1_outdim"],
-                                               k2_outdim=layer_cfg["k2_outdim"],)
-        
-        params = sum(p.numel() for p in predictor.parameters())
-        print(f"Layer {layer} params: {params}")
-        total_params += params
-        print(predictor)
-        break
+    weight = torch.load(os.path.join(args.predictor_path, "predictor.pth"))
+
+    # 计算显存占用（单位：MB）
+    total_bytes = 0
+    for k, v in weight.items():
+        if isinstance(v, torch.Tensor):
+            total_bytes += v.numel() * v.element_size()
+
+    total_mb = total_bytes * (4+4+2) / (1024 ** 2)
+    print(f"Total memory: {total_mb:.2f} MB")
     
-    total_memory = total_params * 2 / (1024 ** 2)  # Convert to MB
-    
-    print(f"Total memory: {total_memory:.2f} MB")
+   
         
